e45aa6a1097e3e8542e6ee5853b7a2b425b6d6f3
Ali Hammoud <157814679+Ali-Hammoud-12@users.noreply.github.com>
2024-07-10 14:38:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -110,5 +110,5 @@ app.get('/', (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running at 13.48.106.84:${port}`);
 });
